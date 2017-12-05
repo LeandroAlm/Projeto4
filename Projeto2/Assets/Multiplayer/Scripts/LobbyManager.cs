fbe8b99099c89b4df558b776d2f63a3e14a5ee4f
GoncaloRego <goncalo-fcp@hotmail.com>
2017-12-05 02:14:54 +0000

    Menu Scene & Multiplayer Scene
    já abre as várias cenas que vamos ter. no single player abre a cena normal, no multiplayer abre uma cena em que fica à espera que outro jogador se conecte e pode voltar atrás caso se engane

---
+++ Projeto2/Assets/Multiplayer/Scripts/LobbyManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public GameObject readyButton, exitButton, cancelButton, searchingPlayersText, sliderGameObject;
+    public Slider loadingSlider;
+
+	void Start ()
+	{
+	    searchingPlayersText.GetComponent<Text>();
+	    readyButton.GetComponent<Button>().onClick.AddListener(PlayerReady);
+	    exitButton.GetComponent<Button>().onClick.AddListener(ExitScene);
+	    cancelButton.GetComponent<Button>().onClick.AddListener(CancelScene);
+
+        searchingPlayersText.SetActive(false);
+
+	    sliderGameObject.SetActive(false);
+
+        cancelButton.SetActive(false);
+        
+	}
+	
+	void Update ()
+    {
+        loadingSlider.value += 0.01f;
+
+        if (loadingSlider.value >= 1)
+        {
+            loadingSlider.value = 0;
+        }
+    }
+
+    public void PlayerReady()
+    {
+        readyButton.SetActive(false);
+        exitButton.SetActive(false);
+        cancelButton.SetActive(true);
+        searchingPlayersText.SetActive(true);
+        sliderGameObject.SetActive(true);
+    }
+
+    public void ExitScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void CancelScene()
+    {
+        SceneManager.LoadScene("MultiplayerScene");
+    }
+}
---
+++ Projeto2/Assets/StartMenu/Scripts/StartMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    public void StartSinglePlayer()
+    {
+        SceneManager.LoadScene("Cena");
+    }
+
+    public void StartMultiplayer()
+    {
+        SceneManager.LoadScene("MultiplayerScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
