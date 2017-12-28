2145056115c18de0fa7b4924e8408e1c44a5d254
Unknown <caldas_96@live.com.pt>
2017-12-28 18:42:11 +0000

    Werewolf/Wolf/Anim

---
+++ Projeto2/Assets/Werewolf/WEREWOLF/FBX Files/WerewolfAnimController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WerewolfAnimController : MonoBehaviour {
+
+
+    private Animator animator;
+
+    float timer = 1;
+
+
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update ()
+    {
+       
+    }
+
+    void Death()
+    {
+        animator.SetBool("death", true);
+        
+    }
+
+    void Roar()
+    {
+        animator.SetBool("roar", true);
+
+    }
+
+    void Walk()
+    {
+        animator.SetBool("walk", true);
+
+    }
+
+    void Run()
+    {
+        animator.SetBool("run", true);
+
+    }
+
+    void WalkAttack()
+    {
+        animator.SetBool("walkAttack", true);
+
+    }
+
+    void WallDestroy()
+    {
+        animator.SetBool("peNaPorta", true);
+
+    }
+
+    void Attack()
+    {
+        float attackType;
+
+        if (timer <= 0)
+        {
+            attackType = Random.Range(0.1f, 0.9f);
+
+            animator.SetBool("attack", true);
+
+            animator.SetFloat("attackFloat", attackType, 0.1f, Time.deltaTime);
+
+            timer = 1;
+            
+        }
+        timer -= Time.deltaTime;
+
+        
+    }
+}
---
+++ Projeto2/Assets/Wolfs/FBX Files/WolfAnimController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAnimController : MonoBehaviour {
+
+    private Animator animator;
+
+    public AudioClip howlSound;
+
+    private AudioSource audioSource;
+
+
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+ 
+    void Update ()
+    {
+    }
+
+    void Run()
+    {
+        animator.SetBool("run", true);
+    }
+
+    void IdleAttack()
+    {
+        animator.SetBool("standbite", true);    
+    }
+
+    void RunAttack()
+    {
+        animator.SetBool("runBite", true);
+    }
+
+    void Death()
+    {
+        animator.SetBool("dead", true);
+    }
+
+    void Gethit()
+    {
+        animator.SetBool("gethit", true);
+    }
+
+    void HowlSound()
+    {
+        audioSource.Play();
+
+    }
+}
